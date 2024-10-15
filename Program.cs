45331c5978e19bfabe0f589802b26895a79b92bc
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-10-15 08:02:20 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿//*****************************************************************************
+//** 2938. Separate Black and White Balls    leetcode                        **
+//*****************************************************************************
+
+
+long long minimumSteps(char* s) {
+    long long res = 0;
+    long long swaps = 0;
+    int len = strlen(s);
+
+    for (int i = 0; i < len; i++)
+    {
+        if (s[i] == '1')
+        {
+            swaps++;
+        }
+        else
+        {
+            res += swaps;
+        }
+    }
+
+    return res;
+}
